93251af131e282c9c2401e4ffa8d8fc9f1b10f39
youngpar <youngseo321@gmail.com>
2023-12-04 14:49:25 +0900

    Init

---
+++ app/src/main/java/com/tistory/chefgpt/ChefgptApplication.java
@@ -0,0 +1,11 @@
+package com.tistory.chefgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefgptApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ChefgptApplication.class, args);
+	}
+}
---
+++ app/src/test/java/com/tistory/chefgpt/ChefgptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.chefgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefgptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
